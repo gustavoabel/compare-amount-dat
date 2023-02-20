ee92cce8d50b91b1141360c74c2bff037e9506e1
gustavoabel <contato.gustavoabel@gmail.com>
2023-02-20 15:33:35 -0300

    feat: extract objects ids

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const fs = require('fs');
 
 const file1 = JSON.parse(fs.readFileSync('./file1.json', 'utf8'));
 const file2 = JSON.parse(fs.readFileSync('./file2.json', 'utf8'));
+
+const data1 = JSON.parse(file1);
+const data2 = JSON.parse(file2);
+const ids1 = data1.map(obj => obj.id);
+const ids2 = data2.map(obj => obj.id);
+
